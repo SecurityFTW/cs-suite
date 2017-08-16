ff4e8d9fd9fd68c57bd5a57e75543f0bdcd05fe7
shivankar-madaan <shivankarmadaan@gmail.com>
2017-08-16 17:39:02 +0530

    deleting test file

--- test.py
+++
@@ -1,6 +0,0 @@
-import webbrowser
-import os
-cool = 'aws_audit'
-var = os.path.realpath('./reports/%s/sprink-qa/%s/delta/cdn.html' %(cool,'20170814-223844'))
-print var
-webbrowser.open('file://'+(var))
