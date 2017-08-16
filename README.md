6c8fa313b970899bf838f0a16e03b2bf31971272
Ajith Chandran <ajithredsmoke@gmail.com>
2017-08-16 19:29:43 +0530

    Added isolation of venv files

--- README.md
+++ README.md
@@ -5,7 +5,22 @@
 	cd cs-suite/
 	python setup.py
 
+### Virtual Environment installation
+   (So you don't mess with the already installed python libraries)
+   
+ - `pip install virtualenvironmentwrapper`
+ - add it to the respective rc file of your shell (bashrc/zshrc) (for fish shell users check virtualfish - https://github.com/adambrenecki/virtualfish)
+ - source the file 
+    `source ~/.bashrc`
+ - `cd cs-suite/` 
+ -  `mkvirtualenv cssuite`
+ -  `workon cssuite`
+ - `pip install -r requirements-virtual.txt`
+ - `aws configure`  
+
 Once installation is done, the tool will ask you for the AWS keys and region. These two are mandatory for the tool to work.
 
 ## Run ##
 	python cs.py
+
+
--- setup.py
+++ setup.py
@@ -5,12 +5,8 @@ import subprocess
 def main():
     """ main function """
     try:
-        print ("installing requirements for Scout2")
-        subprocess.call(['sudo', 'pip', 'install', '-r', './tools/Scout2/requirements.txt'])
-        print ("Setting up Scout2")
-        subprocess.call(['python', 'setup.py', 'install'], cwd='tools/Scout2')
-        print ("Installing other requirements")
-        subprocess.call(['sudo', 'pip', 'install', '-r', './scripts/requirements.txt'])
+        print ("Installing dependencies...")
+        subprocess.call(['sudo', 'pip', 'install', '-r', 'requirements.txt'])
         print ("Installing AWS CLI") # WINDOWS HAS NO SUDO 
         subprocess.call(['sudo', 'pip', 'install', 'awscli', '--ignore-installed', 'six'])
         print ("Please enter your AWS credetionals")
