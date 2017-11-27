d6112892a5acfc26ee614cee481ebc0a31e75042
shivankar-madaan <shivankarmadaan@gmail.com>
2017-11-27 20:49:06 +0530

    Adding support for sshpass installation

--- setup.py
+++ setup.py
@@ -7,8 +7,12 @@ def main():
     try:
         print ("Installing dependencies...")
         subprocess.call(['sudo', 'pip', 'install', '-r', 'requirements.txt'])
-        print ("Installing AWS CLI") # WINDOWS HAS NO SUDO 
+        print ("Installing AWS CLI") # WINDOWS HAS NO SUDO
         subprocess.call(['sudo', 'pip', 'install', 'awscli', '--ignore-installed', 'six'])
+        print ("Installing sshpass")
+        subprocess.call(['tar', '-xvf', 'sshpass-1.06.tar.gz'], cwd = './tools')
+        subprocess.check_output(['./configure'], cwd='tools/sshpass-1.06')
+        subprocess.call(['sudo', 'make', 'install'], cwd='tools/sshpass-1.06/')
         print ("Please enter your AWS credetionals")
         subprocess.call(['aws', 'configure'])
     except:
