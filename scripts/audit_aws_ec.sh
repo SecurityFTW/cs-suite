ebb8368f41e7c5c9f2e1ecab34254c52adeca9e5
shivankarmadaan <shivankar.madaan@sprinklr.com>
2018-03-01 21:53:49 +0530

    added json output

--- scripts/audit_aws_ec.sh
+++ scripts/audit_aws_ec.sh
@@ -11,6 +11,7 @@
   #printf "${BOLD}############\n"
   #printf " ElastiCache AUDIT\n"
   #printf "############${NC}\n\n"
+  account=`aws sts get-caller-identity --output text --query 'Account'`
 for  aws_region in ap-south-1 eu-west-2 eu-west-1 ap-northeast-2 ap-northeast-1 sa-east-1 ca-central-1 ap-southeast-1 ap-southeast-2 eu-central-1 us-east-1 us-east-2 us-west-1 us-west-2;do
   caches=`aws elasticache describe-replication-groups --region $aws_region --query 'ReplicationGroups[].ReplicationGroupId' --output text` 
   for cache in $caches; do 
--- scripts/audit_aws_ec2.sh
+++ scripts/audit_aws_ec2.sh
@@ -19,7 +19,7 @@
 for  aws_region in ap-south-1 eu-west-2 eu-west-1 ap-northeast-2 ap-northeast-1 sa-east-1 ca-central-1 ap-southeast-1 ap-southeast-2 eu-central-1 us-east-1 us-east-2 us-west-1 us-west-2;do
   check=`aws ec2 describe-instances --region $aws_region --query 'Reservations[*].Instances[?!IamInstanceProfile==\`true\`].InstanceId[]' --output text`
   if [[ ! -z $check ]];then
-      checkb=`aws ec2 describe-instances --region $aws_region --query 'Reservations[*].Instances[?!IamInstanceProfile==\`true\`].InstanceId[]'`
+      checkb=`aws ec2 describe-instances --region $aws_region --query 'Reservations[*].Instances[?!IamInstanceProfile==\`true\`].InstanceId[]' --output text`
       printf "default,$account,$aws_region,null,WARNING,Scored,null,EC2_AUDIT,List of servers which are not associated with IamInstanceProfile $check \n"
   fi
   public=`aws ec2 describe-images --region $aws_region --owners self --query 'Images[?Public==\`true\`].ImageId' --output text`
@@ -28,12 +28,12 @@ for  aws_region in ap-south-1 eu-west-2 eu-west-1 ap-northeast-2 ap-northeast-1 
     fi
   check=`aws ec2 describe-volumes --region $aws_region --query 'Volumes[?Encrypted==\`false\`].VolumeId' --output text`
   if [[ ! -z $check ]];then
-      checkb=`aws ec2 describe-volumes --region $aws_region --query 'Volumes[?Encrypted==\`false\`].VolumeId'`
+      checkb=`aws ec2 describe-volumes --region $aws_region --query 'Volumes[?Encrypted==\`false\`].VolumeId' --output text`
       printf "default,$account,$aws_region,null,WARNING,Scored,null,EC2_AUDIT,List of volumes which are not encrypted $checkb\n"
   fi
   kms=`aws ec2 describe-volumes --region $aws_region --query 'Volumes[?!KmsKeyId==\`true\`].VolumeId' --output text`
   if [[ ! -z $kms ]];then
-      kmsb=`aws ec2 describe-volumes --region $aws_region --query 'Volumes[?!KmsKeyId==\`true\`].VolumeId'`
+      kmsb=`aws ec2 describe-volumes --region $aws_region --query 'Volumes[?!KmsKeyId==\`true\`].VolumeId' --output text`
       printf "default,$account,$aws_region,null,WARNING,Scored,null,EC2_AUDIT,List of volumes which are not encrypted with KMS key are $kmsb\n"
   fi
 done
